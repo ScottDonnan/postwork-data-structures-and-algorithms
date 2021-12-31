97ca6501ec54117f53ab056b37ed1c69a7b0add9
Scott Donnan <scott.donnan@me.com>
2021-12-31 10:43:40 -0600

    finished intro

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/ruby/fibonacci.rb
@@ -1,5 +1,15 @@
 def fibonacci(num)
-  # type your code in here
+  fib = Array.new
+  for i in 0..num do
+    if i == 0
+      fib[0] = 0
+    elsif i == 1
+      fib[1] = 1
+    else
+      fib[i] = fib[i-2] + fib[i-1]
+    end
+  end
+  fib[num]
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -9,12 +19,15 @@ if __FILE__ == $PROGRAM_NAME
   puts
 
   puts "Expecting: 1"
-  puts "=>", fibonacci(2)
+  puts "=>", fibonacci(9)
 
   puts
 
   puts "Expecting: 55"
   puts "=>", fibonacci(10)
+
+  puts "Expecting: ?"
+  puts "=>", fibonacci(11)
 
   # Don't forget to add your own!
 end
