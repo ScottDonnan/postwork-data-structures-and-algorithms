d592ff9eec13a9863587544619d54994399e49ea
Scott Donnan <scott.donnan@me.com>
2022-01-07 13:52:43 -0600

    solutions

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,30 @@
 function romanNumeral(string) {
-  // type your code here
+  romanNumObj = {
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500,
+    "M":1000
+  }
+
+  let numVal = 0;
+
+  for (i=0; i<string.length; i++) {
+    let current = romanNumObj[string[i]];
+    let previous = romanNumObj[string[i - 1]];
+    let added = 0;
+
+    if (current > previous) {
+      added = current - previous
+      numVal += added - previous
+    } else {
+      numVal += current
+    }
+  }
+
+  return numVal;
 }
 
 if (require.main === module) {
@@ -21,4 +46,13 @@ if (require.main === module) {
 module.exports = romanNumeral;
 
 // Please add your pseudocode to this file
+    // define variable to hold sum  
+    // for (loop through string characters) {
+    //   find value of character from Object
+    //   add character value to summing variable
+    //     if character + 1 is greater than previous character subtract
+    //     two characters before adding to sum variable
+
+    // return sum variable
+  
 // And a written explanation of your solution
