b960735bb75c239686c7aec9511bf1ec8b394fe9
Scott Donnan <scott.donnan@me.com>
2022-04-01 13:48:05 -0500

    set solution

--- 06-week-5--big-o-continued/02-day-5--implement-a-set/javascript/my_set.js
+++ 06-week-5--big-o-continued/02-day-5--implement-a-set/javascript/my_set.js
@@ -3,87 +3,94 @@ class MySet {
   // if an iterable is provided only its unique values should be in data
   // strings and arrays will need to be broken down by their elements/characters
   constructor(iterable) {
-    this.data = {};
-    if (!(typeof iterable === 'string' || iterable === undefined || Array.isArray(iterable))) {
-      throw new Error("can't initialize")
-    }
+    if (typeof iterable === 'string' || Array.isArray(iterable) || iterable === undefined) {
+      this.data = {};
 
-    let size;
-    iterable !== undefined ? size = iterable.length : size = 0
+      if(iterable) {
+        for (let i of iterable) {
+          this.data[i] = true;
+        }
+      }
+
+    } else {
+      return 'error'
+    }
     //loop through each iterable
     //check if iterable value in this.data === true
     //if not add key value pair
-    for (let i = 0; i < size; i++) {
-      this.data[iterable[i]] = true
-    }
+    
   }
 
   // return number of elements in MySet
   size() {
-    const keys = Object.keys(this.data)
-    return keys.length
-    
+    return Object.keys(this.data).length
   }
 
   // add an item to MySet as is
   // don't worry about arrays here!
   // return the MySet instance
   add(item) {
-    this.data[item] = true
-    return this
+    this.data[item] = true;
+    return this;
   }
 
   // delete an item from MySet
   // don't worry about arrays here!
   // return true if successful, otherwise false
   delete(item) {
-    if(this.data[item]) {
-      return delete this.data[item]
+    if (this.data[item]) {
+      delete this.data[item];
+      return true;
     } else {
-      return false
+      return false;
     }
   }
 
   // return true if in MySet, otherwise false
   // don't worry about arrays here!
   has(item) {
-    return !!this.data[item]
+    if(this.data[item]) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   // return data as an array
   // don't worry about arrays here!
   entries() {
-    return Object.keys(this.data)
+    let result = [];
+    for (let key in this.data) {
+      result.push(key)
+    }
+    return result;
   }
 }
 
 if (require.main === module) {
   // add your own tests in here
   const testString = new MySet('pinaple')
-  const testArray = new MySet([1,2,2,3,3,4,5,6,6])
-  const testNil = new MySet()
+  
+  console.log(testString)
+  console.log(' ')
 
+  console.log(testString.size())
+  console.log(' ')
+
+  console.log(testString.add([1,2]))
+  console.log(' ')
+
+  console.log(testString.delete('a'))
+  console.log(' ')
 
   console.log(testString)
-  console.log(testArray)
-  console.log(testNil)
+  console.log(' ')
 
-  console.log('')
+  console.log(testString.has('10'))
+  console.log(' ')
 
-  console.log(testArray.size())
-
-  console.log('')
-
-  console.log(testArray.add(20))
-
-  console.log('')
-
-  console.log(testArray.delete(20))
-  console.log(testArray.entries())
-  console.log(testArray.has(6))
-  console.log(testArray.has(66))
-
-  
+  console.log(testString.entries())
+  console.log(' ')
 }
 
 module.exports = MySet;
--- 07-week-6--foundational-data-structures/00-days-1-to-2--implement-a-linked-list/javascript/linked_list.js
+++ 07-week-6--foundational-data-structures/00-days-1-to-2--implement-a-linked-list/javascript/linked_list.js
@@ -1,33 +1,60 @@
 class LinkedList {
-  constructor() {
-
+  constructor(head = null) {
+    this.head = head
   }
 
-  iterate() {
+  iterate(another) {
+    let start = this.head
 
+    while (start !== null) {
+      another(start)
+      start = start.next
+    }
+
+    return this.head
   }
 
   // print each node's value on its own line
   // use your iterate method to be DRY! Don't get caught in the code rain, brrr.
   print() {
-
+    // function printNode(node){
+    //   console.log(node.value)
+    // }
+    
+    this.iterate(printNode => console.log(printNode.value))
   }
 
   // find the node with the target value and return it
   // if not found return null, use your iterate method to be DRY!
   find(target) {
+    //iterate through each value and check if it equals the target
+    let findNode = null
+    this.iterate(firstNode => {
+      if (firstNode.value === target) {
+        findNode = firstNode
+      }
+    })
+
+    return findNode;
 
   }
 
   // add the node to the start of the list, no nodes should be removed
   addFirst(node) {
-
+    node.next = this.head
+    this.head = node
   }
 
   // add node to end of list, no nodes should be removed
   // you may wish to use the iterate method
   addLast(node) {
-
+    let last
+    this.iterate(currentNode => {
+      if (currentNode.next === null) {
+        last = currentNode
+      }
+    })
+    last.next = node
   }
 
   // remove the first Node in the list and update head
@@ -60,14 +87,46 @@ class LinkedList {
 }
 
 class Node {
-  constructor() {
-    
+  constructor(value = null, next = null) {
+    this.value = value;
+    this.next = next
   }
 }
 
 if (require.main === module) {
   // add your own tests in here
-  
+  // const node = new Node('scott', new Node('jennifer', new Node('clayton')))
+  // const list = new LinkedList(node)
+  // const drinks = new Node('coffee', new Node('manhattan', new Node('brandy sour')))
+  // const drinkList = new LinkedList(drinks)
+  const head = new Node('hi again', new Node('but why?'))
+  const list = new LinkedList(head)
+  // console.log(node)
+  // console.log(node.value)
+  // console.log(node.next)
+
+  // console.log('')
+
+  // console.log("list head =>", list.head)
+  // console.log(list.head.value)
+  // console.log(list.head.next.next.value)
+
+  // console.log("drink list head =>", drinkList.head)
+  // console.log("drinks =>", drinkList.head.next.next)
+    // console.log(list.head.next.value)
+    // console.log(list.iterate())
+    // console.log(list.print())
+    // console.log(list.find("hi again"))
+    // console.log(list.find("but why?"))
+    // console.log(list.find("test"))
+    // console.log(list.find(456))
+
+    console.log("addFirst =>", list.addFirst(new Node('I am first now')))
+    console.log("print =>", list.print())
+    console.log("addLast =>", list.addLast(new Node('I am last now')))
+    console.log("print =>", list.print())
+
+
 }
 
 module.exports = {
