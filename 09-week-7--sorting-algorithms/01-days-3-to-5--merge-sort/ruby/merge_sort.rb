66f1b652bf6d87bf2ed582b57e2adc6fd3a490ea
Scott Donnan <scott.donnan@me.com>
2022-04-07 14:40:39 -0500

    merge sort solution

--- 09-week-7--sorting-algorithms/01-days-3-to-5--merge-sort/ruby/merge_sort.rb
+++ 09-week-7--sorting-algorithms/01-days-3-to-5--merge-sort/ruby/merge_sort.rb
@@ -1,9 +1,44 @@
 def merge(arr1, arr2)
   # type your code in here
+  # initialize array for merge
+  # while arr1 or arr2 contains some elem
+  # compare arr1[0] and arr2[0]
+    # if arr1 is < arr2
+    # remove arr1[0] from arr and push to result array
+    # otherwise remove and push arr2[0]
+
+  result = [];
+  while arr1.length > 0 && arr2.length > 0
+    if arr1[0] < arr2[0]
+      result.push(arr1.shift)
+    else
+      result.push(arr2.shift)
+    end
+  end
+  
+  result + arr1 + arr2
 end
 
 def merge_sort(arr)
   # type your code in here
+  # reduce arr into 2 sorted arrays of 1 element and then call merge on those 2 arrays
+  # can do this over and over with recursion
+  # arr.length / 2
+  # arr1 = from arr[0] to arr.length/2
+
+  #base case
+  if arr.length < 2
+    return arr
+  end
+  
+  middle = arr.length/2
+  left = merge_sort(arr[0...middle])
+  right = merge_sort(arr[middle..arr.length-1])
+
+  # left = merge_sort(arr[0...middle])
+  # right = merge_sort(arr[middle..-1])
+
+  merge(left, right)
 end
 
 if __FILE__ == $PROGRAM_NAME
